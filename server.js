07c38133e2380a4d4a5409ab3a922e957c59f517
Arun Kumar M <arun2451.a2@gmail.com>
2025-03-11 20:55:07 +0530

    Added Fivem Playing Count

--- server.js
+++ server.js
@@ -3,12 +3,15 @@ import { Client, GatewayIntentBits } from 'discord.js';
 import { WebSocketServer } from 'ws';
 import express from 'express';
 import { createServer } from 'http';
+import axios from 'axios';
+import cors from 'cors';
 
 // Declare Port
 const PORT = process.env.PORT;
 
 // Express.JS & Websocket
 const app = express();
+app.use(cors());
 const server = createServer(app);
 const wss = new WebSocketServer({ server });
 const UPDATE_INTERVAL = 5000;
@@ -33,9 +36,34 @@ const ALLOWED_ROLES = [
   'ADVENTURER',
 ];
 
+const FIVEM_SERVER_IP = '4xeqgv';
+
 client.once('ready', () => {
   console.log(`Logged in as ${client.user.tag}`);
 });
+
+async function getFivemData(FIVEM_SERVER_IP) {
+  try {
+    const response = await axios.get(
+      `https://servers-frontend.fivem.net/api/servers/single/${FIVEM_SERVER_IP}`,
+      {
+        headers: {
+          'User-Agent':
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36',
+          Accept: 'application/json',
+        },
+      }
+    );
+    return response.data;
+  } catch (error) {
+    console.error(
+      '❌ Error fetching FiveM data:',
+      error.response?.status,
+      error.response?.statusText
+    );
+    return 'Error';
+  }
+}
 
 // Get total member count from discord server
 async function getMemberCount(guildId) {
@@ -73,15 +101,17 @@ async function getFilteredRoleCounts(guildId) {
 }
 
 // Calling all the functions Above
-async function getDiscordData() {
+async function getAllData() {
   try {
     const guild = await getMemberCount(SERVER_ID);
     const onlineCount = await getOnlineMembers(SERVER_ID);
     const rolesBasedCount = await getFilteredRoleCounts(SERVER_ID);
+    const getFivemCount = await getFivemData(FIVEM_SERVER_IP);
     return {
       count: guild.memberCount,
       onlineCount: onlineCount,
       rolesBasedCount: rolesBasedCount,
+      getFivemCount: getFivemCount
     };
   } catch (error) {
     return { error: 'Error fetching member count' };
@@ -93,11 +123,12 @@ wss.on('connection', (ws) => {
   console.log('Client connected');
 
   const sendUpdates = async () => {
-    const data = await getDiscordData();
+    const data = await getAllData();
     ws.send(JSON.stringify(data));
   };
 
   sendUpdates();
+  // getFivemData(FIVEM_SERVER_IP);
   const interval = setInterval(sendUpdates, UPDATE_INTERVAL);
 
   ws.on('close', () => {
